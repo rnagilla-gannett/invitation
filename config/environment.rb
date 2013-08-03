e1a4cb26de0b08594bbc1bc995334b761ea62947
rnagilla <rnagilla.gannett@gmail.com>
2013-08-03 14:06:55 -0400

    hide gmail password

--- config/environment.rb
+++ config/environment.rb
@@ -17,8 +17,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "supriyaandrakesh",
-  :password             => "August25th2013",
+  :user_name            => ENV['GMAIL_SMTP_USER'],
+  :password             => ENV['GMAIL_SMTP_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
